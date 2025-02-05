472f9d699111561862acc2dcd60f46744522b67b
lucagallotta95 <lucagallotta95@gmail.com>
2025-02-05 23:55:54 +0100

    Aggiunta entity SubReddit

---
+++ demo6/src/main/java/org/generation/jaita138/demo6/demo6/db/entity/SubReddit.java
@@ -0,0 +1,52 @@
+package org.generation.jaita138.demo6.demo6.db.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class SubReddit {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(length = 64)
+    private String nome;
+
+    @Column(columnDefinition = "TEXT")
+    private String descrizione;
+    
+    public Long getId() {
+        return id;
+    }
+
+
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getDescrizione() {
+        return descrizione;
+    }
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+
+    @Override
+    public String toString() {
+        return "SubReddit [id=" + id + ", nome=" + nome + ", descrizione=" + descrizione + "]";
+    }
+
+    
+
+}
